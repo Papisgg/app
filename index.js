df30d3f0c79ab9b9c60d9ce097f1e50457592686
Papisgg <165417679+Papisgg@users.noreply.github.com>
2024-05-21 02:57:20 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,101 @@
+document.getElementById("btn-write").addEventListener("click", function() {
+    document.getElementById('modal').classList.add('open')
+})
+
+document.getElementById("close").addEventListener("click", function() {
+    document.getElementById('modal').classList.remove('open')
+})
+
+document.querySelector("#modal .modal-content").addEventListener("click", event => {
+    event._isClickInModal = true;
+})
+
+document.getElementById("modal").addEventListener('click', event => {
+    if (event._isClickInModal) return;
+    event.currentTarget.classList.remove("open");
+})
+
+document.getElementById("btn-write1").addEventListener("click", function() {
+    document.getElementById('modal').classList.add('open')
+})
+
+document.getElementById("close").addEventListener("click", function() {
+    document.getElementById('modal').classList.remove('open')
+})
+
+document.querySelector("#modal .modal-content").addEventListener("click", event => {
+    event._isClickInModal = true;
+})
+
+document.getElementById("modal").addEventListener('click', event => {
+    if (event._isClickInModal) return;
+    event.currentTarget.classList.remove("open");
+})
+
+document.getElementById("btn-write2").addEventListener("click", function() {
+    document.getElementById('modal').classList.add('open')
+})
+
+document.getElementById("close").addEventListener("click", function() {
+    document.getElementById('modal').classList.remove('open')
+})
+
+document.querySelector("#modal .modal-content").addEventListener("click", event => {
+    event._isClickInModal = true;
+})
+
+document.getElementById("modal").addEventListener('click', event => {
+    if (event._isClickInModal) return;
+    event.currentTarget.classList.remove("open");
+})
+
+document.getElementById("btn-write3").addEventListener("click", function() {
+    document.getElementById('modal').classList.add('open')
+})
+
+document.getElementById("close").addEventListener("click", function() {
+    document.getElementById('modal').classList.remove('open')
+})
+
+document.querySelector("#modal .modal-content").addEventListener("click", event => {
+    event._isClickInModal = true;
+})
+
+document.getElementById("modal").addEventListener('click', event => {
+    if (event._isClickInModal) return;
+    event.currentTarget.classList.remove("open");
+})
+
+document.getElementById("btn-write4").addEventListener("click", function() {
+    document.getElementById('modal').classList.add('open')
+})
+
+document.getElementById("close").addEventListener("click", function() {
+    document.getElementById('modal').classList.remove('open')
+})
+
+document.querySelector("#modal .modal-content").addEventListener("click", event => {
+    event._isClickInModal = true;
+})
+
+document.getElementById("modal").addEventListener('click', event => {
+    if (event._isClickInModal) return;
+    event.currentTarget.classList.remove("open");
+})
+
+document.getElementById("btn-write5").addEventListener("click", function() {
+    document.getElementById('modal').classList.add('open')
+})
+
+document.getElementById("close").addEventListener("click", function() {
+    document.getElementById('modal').classList.remove('open')
+})
+
+document.querySelector("#modal .modal-content").addEventListener("click", event => {
+    event._isClickInModal = true;
+})
+
+document.getElementById("modal").addEventListener('click', event => {
+    if (event._isClickInModal) return;
+    event.currentTarget.classList.remove("open");
+})
