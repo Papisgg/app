7f6b6e5f182556b578334b2df4fd6420da15032f
Papisgg <165417679+Papisgg@users.noreply.github.com>
2024-05-28 21:38:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,101 +1,40 @@
-document.getElementById("btn-write").addEventListener("click", function() {
-    document.getElementById('modal').classList.add('open')
+//like-btn + modal
+const likeBtns = document.querySelectorAll('.like-btn');
+const likeModal = document.getElementById('like-modal');
+
+let isActive = false;
+
+likeBtns.forEach((likeBtn) => {
+    likeBtn.addEventListener('click', function() {
+        isActive = !isActive;
+
+        if (isActive) {
+            likeBtn.classList.add('active');
+            likeModal.classList.add('open');
+
+            setTimeout(() => {
+                likeModal.classList.remove('open');
+            }, 2000);
+        } else {
+            likeBtn.classList.remove('active');
+        }
+    });
+});
+
+document.getElementById("more").addEventListener("click", function() {
+    document.getElementById('more-modal').classList.add('open')
 })
 
 document.getElementById("close").addEventListener("click", function() {
-    document.getElementById('modal').classList.remove('open')
+    document.getElementById('more-modal').classList.remove('open')
 })
 
-document.querySelector("#modal .modal-content").addEventListener("click", event => {
+document.querySelector("#more-modal .more-modal-content").addEventListener("click", event => {
     event._isClickInModal = true;
 })
 
-document.getElementById("modal").addEventListener('click', event => {
+document.getElementById("more-modal").addEventListener('click', event => {
     if (event._isClickInModal) return;
     event.currentTarget.classList.remove("open");
 })
 
-document.getElementById("btn-write1").addEventListener("click", function() {
-    document.getElementById('modal').classList.add('open')
-})
-
-document.getElementById("close").addEventListener("click", function() {
-    document.getElementById('modal').classList.remove('open')
-})
-
-document.querySelector("#modal .modal-content").addEventListener("click", event => {
-    event._isClickInModal = true;
-})
-
-document.getElementById("modal").addEventListener('click', event => {
-    if (event._isClickInModal) return;
-    event.currentTarget.classList.remove("open");
-})
-
-document.getElementById("btn-write2").addEventListener("click", function() {
-    document.getElementById('modal').classList.add('open')
-})
-
-document.getElementById("close").addEventListener("click", function() {
-    document.getElementById('modal').classList.remove('open')
-})
-
-document.querySelector("#modal .modal-content").addEventListener("click", event => {
-    event._isClickInModal = true;
-})
-
-document.getElementById("modal").addEventListener('click', event => {
-    if (event._isClickInModal) return;
-    event.currentTarget.classList.remove("open");
-})
-
-document.getElementById("btn-write3").addEventListener("click", function() {
-    document.getElementById('modal').classList.add('open')
-})
-
-document.getElementById("close").addEventListener("click", function() {
-    document.getElementById('modal').classList.remove('open')
-})
-
-document.querySelector("#modal .modal-content").addEventListener("click", event => {
-    event._isClickInModal = true;
-})
-
-document.getElementById("modal").addEventListener('click', event => {
-    if (event._isClickInModal) return;
-    event.currentTarget.classList.remove("open");
-})
-
-document.getElementById("btn-write4").addEventListener("click", function() {
-    document.getElementById('modal').classList.add('open')
-})
-
-document.getElementById("close").addEventListener("click", function() {
-    document.getElementById('modal').classList.remove('open')
-})
-
-document.querySelector("#modal .modal-content").addEventListener("click", event => {
-    event._isClickInModal = true;
-})
-
-document.getElementById("modal").addEventListener('click', event => {
-    if (event._isClickInModal) return;
-    event.currentTarget.classList.remove("open");
-})
-
-document.getElementById("btn-write5").addEventListener("click", function() {
-    document.getElementById('modal').classList.add('open')
-})
-
-document.getElementById("close").addEventListener("click", function() {
-    document.getElementById('modal').classList.remove('open')
-})
-
-document.querySelector("#modal .modal-content").addEventListener("click", event => {
-    event._isClickInModal = true;
-})
-
-document.getElementById("modal").addEventListener('click', event => {
-    if (event._isClickInModal) return;
-    event.currentTarget.classList.remove("open");
-})
